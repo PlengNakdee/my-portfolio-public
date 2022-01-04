a9dcb26b451713e20b9f43d0f37fb025d40d90f6
Pleng Nakdee <plengwn@gmail.com>
2022-01-04 23:07:16 +0700

    Create aws-export.js

---
+++ aws-export.js
@@ -0,0 +1,8 @@
+const config = {
+    aws_appsync_graphqlEndpoint: 'YOUR-ENDPOIT',
+    aws_appsync_region: 'YOUR-REGION',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: 'YOUR-APIKEY',
+  };
+  
+  export default config;
