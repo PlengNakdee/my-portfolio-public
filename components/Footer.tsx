08e1b2fabeb32f45eaed94ed5ff052f8389bb204
Pleng Nakdee <plengwn@gmail.com>
2022-01-04 22:44:29 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  const today = new Date();
+  return (
+    <div className="ml-4 mb-4 mt-10 sm:ml-10 lg:ml-20 text-sm">
+      <p>© {today.getFullYear()} Pleng Nakdee</p>
+    </div>
+  );
+};
+
+export default Footer;
